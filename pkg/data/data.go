5e4d4c2c574db1e57cb29451ac65722170390fa2
Jonas Hoang <bangbang3951@gmail.com>
2023-05-05 09:53:07 +0700

    feat: update rest api

--- pkg/data/data.go
+++ pkg/data/data.go
@@ -9,9 +9,9 @@ var Todos []dto.Todo
 func init() {
 	Todos = []dto.Todo{
 		{ID: 1, Name: "Monday", Content: "Coding GoLang Suong suong"},
-		{ID: 2, Name: "Tuesday", Content: "Coding GoLang Suong suong"},
-		{ID: 1, Name: "Wenesday", Content: "Coding GoLang Suong suong"},
-		{ID: 1, Name: "Thurday", Content: "Coding GoLang Suong suong"},
-		{ID: 1, Name: "Friday", Content: "Coding GoLang Suong suong"},
+		{ID: 2, Name: "Tuesday", Content: "Testing API"},
+		{ID: 3, Name: "Wenesday", Content: "Post get delete ..."},
+		{ID: 4, Name: "Thurday", Content: "Migrate database"},
+		{ID: 5, Name: "Friday", Content: "Deploying GoLang"},
 	}
 }
